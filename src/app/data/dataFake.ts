3bb248949ce43cbfc83dd8caac5a1473ddc6411d
carlosmochi <carlosamochi@gmail.com>
2023-09-20 20:05:38 -0400

    added dynamic homepage card display and hidden excess text from main article

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": 1,
+        "photo": "https://s3-ap-south-1.amazonaws.com/trt-blog-ghost/2023/01/AngularJS.png",
+        "title": "Saiu uma nova versão do angular.JS",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed venenatis urna ac lobortis dapibus. Integer ut lobortis erat. Vivamus vel nibh nec urna iaculis finibus eu ac enim. Aenean bibendum rhoncus est, ut convallis tellus mollis id. Sed placerat lacus eget massa tempor tincidunt. Phasellus commodo tempor lectus at molestie. Mauris sed vulputate risus. Nunc vitae justo magna. Nam euismod iaculis lectus, a fringilla risus pretium ut. Fusce egestas vehicula sem, non auctor neque rhoncus venenatis. Nunc porttitor sapien et sem gravida, eleifend semper erat efficitur. Aliquam sed nisi eu quam ullamcorper hendrerit quis at nulla. Nulla quis lorem neque. Nulla ac sapien purus. Vivamus suscipit quam ut arcu malesuada, vel eleifend arcu bibendum. Donec facilisis tristique malesuada."
+    },
+    {
+        "id": 2,
+        "photo": "https://i.stack.imgur.com/luaq2.png",
+        "title": "Novos dados sobre Angular x Angular.JS",
+        "description": "Integer sed mi sed magna rutrum semper et ac neque. Vestibulum et ex purus. Suspendisse ullamcorper erat venenatis aliquam tempus. Nulla pharetra dui eget lorem feugiat convallis. Donec interdum luctus leo, sed eleifend ante mollis ac. Aenean eget risus semper, rutrum leo at, dignissim enim. Sed suscipit nulla id nulla condimentum viverra. Aliquam egestas malesuada nisi, eu efficitur diam ultrices efficitur. Sed maximus orci felis, ut vehicula purus bibendum sit amet. Cras faucibus iaculis mauris et tincidunt."
+    },
+    {
+        "id": 3,
+        "photo": "https://massivepixel.io/wp-content/uploads/2022/02/react-latest-version-hero-header-1400x583.png",
+        "title": "Nova Versão do React remove suporte para versões legado de navegadores",
+        "description": "Integer leo dui, auctor ac cursus dictum, cursus vel elit. Aliquam viverra ultricies nibh id congue. Praesent nec metus dictum, placerat leo euismod, dapibus mi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse posuere metus et condimentum maximus. Etiam suscipit enim ut nisi mollis tincidunt. Vestibulum suscipit id erat eget pharetra. Phasellus a suscipit nisl, at pharetra eros. Vestibulum fringilla rhoncus faucibus. Donec viverra bibendum lorem non venenatis. Pellentesque consequat, nisi ac lobortis facilisis, sem quam cursus nulla, id dignissim nisl quam vitae nibh. Nullam neque velit, varius vel lacinia ac, faucibus sed erat."
+    },
+    {
+        "id": 4,
+        "photo": "https://relevant.software/wp-content/uploads/2018/04/0_9N9J9YiGJrISLIBP.png",
+        "title": "Vue.js anuncia nova biblioteca standalone que permite integração com outros frameworks SPA",
+        "description": "Aliquam viverra dapibus lorem consequat pharetra. Aliquam ut consectetur sapien, et vestibulum arcu. Sed vitae ligula in nunc commodo molestie. Vestibulum faucibus aliquam leo, quis convallis ligula hendrerit gravida. Pellentesque sagittis convallis sagittis. Proin tincidunt non orci a pharetra. Pellentesque molestie ullamcorper varius."
+    }
+]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  bigCardArticleData:Article = {
+    id: "0",
+    photo: "",
+    title: "",
+    description: ""
+  };
+  smallCardArticleData:Article[] = [];
+
+  constructor(){
+    this.bigCardArticleData = {
+      id: dataFake[0].id.toString(),
+      photo: dataFake[0].photo,
+      title: dataFake[0].title,
+      description: dataFake[0].description
+    };
+    for(let i = 1; i < dataFake.length; i++){
+      this.smallCardArticleData.push({
+        id: dataFake[i].id.toString(),
+        photo: dataFake[i].photo,
+        title: dataFake[i].title,
+        description: dataFake[i].description
+      })
+    }
+    console.log(this.bigCardArticleData);
+    
+  }
+
+  ngOnInit(): void {
+      console.log(this.bigCardArticleData);
+      console.log(this.smallCardArticleData);
+      
+      
+  }
+}
+
+class Article{
+  id:string
+  photo:string
+  title:string
+  description:string
+
+  constructor(id:string, photo:string, title:string, description:string){
+    this.id = id;
+    this.photo = photo;
+    this.title = title;
+    this.description = description;
+  }
 
 }
